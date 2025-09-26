8159551ec31f266db37f5f56af10bc5a040f4e7d
lucas curiel <lcurgom2406@iesmarquesdecomares.org>
2025-09-26 12:57:54 +0200

    apuntes sobre matrises y array y algunos ejemplos

---
+++ Apuntes/arrays.php
@@ -0,0 +1,28 @@
+<?php
+
+$array[5] = "Pepe";
+$array["Nombre"] = "Jose";
+$array[3] = "Maria";
+
+
+
+$array = [2=>"Pepe", 5=>"Juan", "Maria", 1=>"Jose"];
+
+foreach ($array as $ind=>$valor){
+    echo $ind." = ".$valor."<br>";
+}
+
+$array = array(5=>"Pepe", 9=>"Juan", 3=>"Maria");
+
+//Creacion de matrizes
+$matriz[][]="Pepe";
+$matriz[][]="Jose";
+$matriz[][3]="Maria";
+$matriz[][]="Juan";
+$matriz[2][]="Rosa";
+
+$matriz = array(
+    0 =>array("nombre" => "Pepe", "apellido"=> "Carpio", "edad" => 35),
+    1 =>array("nombre" => "Maria", "apellido"=> "Sanchez", "edad" => 28),
+    2 =>array("nombre" => "Juan", "apellido"=> "Ramirez", "edad" => 33)
+    );
---
+++ ejercicios/EjerciciosRapidosDeClase/ejercicioCrearMatriz.php
@@ -0,0 +1,15 @@
+<?php
+
+$matriz = array(
+    "DAW"=>array(
+        "DWES"=>"Diseño web entorno servidor",
+        "DWEC"=>"Diseño web entorno servidor",
+        "DIW"=>"Diseño de ointerdaces web",
+        "OPT"=>"optativa"
+        ),
+    "DAM"=>array(
+        "DI"=>"Diseño de interaces",
+        "Android"=>"Desarrollo en entorno android",
+        "Windows"=>"Desarrollo entorno windows",
+        "OPT"=>"Optativa")
+);

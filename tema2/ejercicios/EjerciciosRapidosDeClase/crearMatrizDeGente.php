3c0ec695392abc1fadb7d564d00a5b5bcd690ab7
Lucas Curiel <lucascuriel44@gmail.com>
2025-09-29 22:47:11 +0200

    ajustaes en el ejecicio de clase

--- tema2/ejercicios/EjerciciosRapidosDeClase/crearMatrizDeGente.php
+++ tema2/ejercicios/EjerciciosRapidosDeClase/crearMatrizDeGente.php
@@ -1,24 +1,33 @@
 <table border='1'>
-<?php 
-
-    $matriz = array(
-        ""array("", "Nombre", "Apellidos", "Salario", "Edad"),
-        array("Marketing", "Pepe", "Lopez", "1500", "35"),
-        array("Contabilidad", "Juan", "Sanchez", "1750", "28"),
-        array("Ventas", "Maria", "Carpio", "1675", "33"),
-        array("Informatica", "Pedro", "Luna", "2100", "48"),
-        array("Direccion", "Rosa", "Catala", "5100", "53"),
+    <?php
+    $datos = array(
+        "" => array("Nombre", "Apellidos", "Salario", "Edad"),
+        "Marketing" => array("Pepe", "Lopez", "1500", "35"),
+        "Contabilidad" => array("Juan", "Sanchez", "1750", "28"),
+        "Ventas" => array("Maria", "Carpio", "1675", "33"),
+        "Informatica" => array("Pedro", "Luna", "2100", "48"),
+        "Direccion" => array("Rosa", "Catala", "5100", "53"),
     );
-    
-    foreach ($matriz as $ind => $depa){
-        echo"<tr>";
-        foreach ($depa as $ind2 => $datos){
-            echo "<td>".$datos."<td>";
-        }
-        echo"</tr>";
-    }
 
-?>
+    echo "<tr>";
+    echo "<th></th>";
+    foreach ($datos[""] as $indi => $filas) {
+        echo"<th>" . $filas . "</th>";
+    }
+    echo "</tr>";
+
+    foreach ($datos as $depa => $valor) {
+        if ($depa == "") {
+            
+        } else {
+            echo "<tr><th>" . $depa . "</th>";
+            foreach ($valor as $dato) {
+                echo"<td>" . $dato . "</td>";
+            }
+            echo "</tr>";
+        }
+    }
+    ?>
 
 </table>
 

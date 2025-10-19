03505a1ad91a7d9cf10a3664228116c43771a638
Lucas Curiel <lucascuriel44@gmail.com>
2025-10-19 20:53:27 +0200

    terminado el formularo de traspasos

--- tema3/ejercicios/ejercicioRapidosDeClase/traspasoDeStock/traspaso.php
+++ tema3/ejercicios/ejercicioRapidosDeClase/traspasoDeStock/traspaso.php
@@ -2,31 +2,67 @@
 
 <form action="" method="POST">
 
+    Teinda de origen
+    <br>
     <select name="tiendaOg">
-        <option value="central">Central</option>
-        <option value="sucursal1">Sucursal1</option>
-        <option value="sucursal2">Sucrusal2</option>
-    </select>
-
-    <select name="tiendaDes">
         <option value="1">Central</option>
         <option value="2">Sucursal1</option>
         <option value="3">Sucrusal2</option>
     </select>
-    <input type="text" name="codigoPro">
-    <input type="number" name="unidades">
-    <input type="submit" name="traspasar">
+    <br><br>
+
+    Tienda de destino
+    <br>
+    <select name="tiendaDes">
+        <option value="1">Central</option>
+        <option value="2">Sucursal1</option>
+        <option value="3">Sucrusal2</option>
+    </select><br>
+    Codigo de procduto 
+    <br>
+    <input type="text" name="codigoPro"><br> 
+    Unidades 
+    <br>
+    <input type="number" name="unidades"><br> 
+
+    <input type="submit" name="traspasar" value="Traspasar"><br>
 
 </form>
 
 <?php
 if (isset($_POST['traspasar'])) {
 
+    mysqli_report(MYSQLI_REPORT_ERROR);
+
     try {
         $conex = new mysqli("localhost", "dwes", "abc123.", "dwes");
-        $conex->query("Update stock set unidades = $_POST['unidades'] where tienda = $_POST['tiendaDes']");
+
+        $tiendaDes = (int) $_POST['tiendaDes'];
+        $tiendaOg = (int) $_POST['tiendaOg'];
+        $producto = $_POST['codigoPro'];
+        $unidades = (int) $_POST['unidades'];
+
+        $conex->autocommit(false);
+        $conex->query("Update stock set unidades = unidades + $unidades where tienda = $tiendaDes and producto= '$producto'");
+        $filasDestino = $conex->affected_rows;
+
+        $conex->query("Update stock set unidades = unidades - $unidades where tienda = $tiendaOg and producto= '$producto' and unidades >= $unidades");
+        $filasOrigen = $conex->affected_rows;
+
+        if ($filasDestino === 0 && $filasOrigen === 0) {
+            $conex->rollback();
+            echo "El producto no existe";
+            
+        } else if ($filasDestino > 0 && $filasOrigen === 0) {
+            $conex ->rollback();
+            echo "No hay suficiente stock";
+            
+        } else {
+            $conex->commit();
+            echo "Traspaso completado";
+        }
     } catch (mysqli_sql_exception $ex) {
-        
+        echo "Erro con la conexion del servidor";
     }
 }
 ?>
